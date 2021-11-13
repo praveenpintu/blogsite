90182a6378ea3738ceecdda4b0742d3dcc80518b
Praveen-PraveenN <nallatigalapraveen1234@gmail.com>
2021-11-13 20:25:59 +0530

    now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import{MatTabsModule} from '@angular/material/tabs'
+import { MatCardModule} from '@angular/material/card';
+import { BioComponent } from './bio/bio.component';
+import { FamilyComponent } from './family/family.component';
+import { ContactComponent } from './contact/contact.component'; 
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BioComponent,
+    FamilyComponent,
+    ContactComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatTabsModule,MatCardModule,MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
